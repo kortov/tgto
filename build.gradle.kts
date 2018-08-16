c8f3f09fd7e422c054ee31d5a7a21339592a90b0
Ruslan Ibragimov <ruslan@ibragimov.by>
2018-08-17 00:05:47 +0300

    Gradle build.

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    application
+    kotlin("jvm") version "1.2.60"
+}
+
+val tgBotsApiVersion: String by project
+val undertowVersion: String by project
+val coroutinesVersion: String by project
+
+val logbackVersion: String by project
+val slf4jVersion: String by project
+val sentryVersion: String by project
+
+val postgresqlVersion: String by project
+val hikariVersion: String by project
+val jooqVersion: String by project
+val flywayVersion: String by project
+
+val junitVersion: String by project
+val mockkVersion: String by project
+
+dependencies {
+    compile(kotlin("stdlib-jdk8"))
+    compile("org.telegram:telegrambots:$tgBotsApiVersion")
+    compile("io.undertow:undertow-core:$undertowVersion")
+    compile("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$coroutinesVersion")
+
+    compile("ch.qos.logback:logback-classic:$logbackVersion")
+    compile("org.slf4j:jul-to-slf4j:$slf4jVersion")
+    compile("io.sentry:sentry-logback:$sentryVersion")
+
+    compile("com.zaxxer:HikariCP:$hikariVersion")
+    compile("org.postgresql:postgresql:$postgresqlVersion")
+    compile("org.jooq:jooq:$jooqVersion")
+    compile("org.jooq:jooq-meta:$jooqVersion")
+    compile("org.jooq:jooq-codegen:$jooqVersion")
+    compile("org.flywaydb:flyway-core:$flywayVersion")
+
+    testCompile("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    testCompile("io.mockk:mockk:$mockkVersion")
+}
+
+repositories {
+    jcenter()
+}
+
+application {
+    applicationName = "tgto"
+    mainClassName = "app.hea.tgto.Application"
+}
