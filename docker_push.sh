e9f78e2cb5586d56d8989b448291f23742bbecee
Ruslan Ibragimov <ruslan@ibragimov.by>
2018-09-13 16:56:29 +0300

    Add some logging to deploy.

--- docker_push.sh
+++ docker_push.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo "-> Login to docker hub"
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 
+echo "-> Pushing image irus/tgto:1.0.0-dev-b${TRAVIS_BUILD_NUMBER} to docker hub"
 docker push irus/tgto
