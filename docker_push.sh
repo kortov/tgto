4628c8877959d9b6201c97f04816cd9cf03c6c17
Ruslan Ibragimov <ruslan@ibragimov.by>
2018-09-24 19:59:30 +0300

    Remove separate deploy script, inline commands to CI configuration.

--- docker_push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo "-> Login to docker hub"
-echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-
-echo "-> Pushing image heapy/tgto:b${TRAVIS_BUILD_NUMBER} to docker hub"
-docker push heapy/tgto
